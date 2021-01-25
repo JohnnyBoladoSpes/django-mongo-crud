5050174d95a5e336f21a629035dabeb46be5c516
Johnny André Bolados Hermosilla <jbolados@amf.cl>
2021-01-25 00:57:20 -0300

    add-mongodb

--- DjangoMongoCrud/DjangoMongoCrudApp/models.py
+++ DjangoMongoCrud/DjangoMongoCrudApp/models.py
@@ -1,3 +1,10 @@
-from django.db import models
-
+#from django.db import models
+from djongo import models
 # Create your models here.
+class Posts(models.Model):
+    id=models.ObjectIdField()
+    post_title=models.CharField(max_length=250)
+    post_description=models.TextField()
+    comment=models.JSONField()
+    tags=models.JSONField()
+    user_details=models.JSONField()
